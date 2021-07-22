bd7e1cfa9bfa9715113d0d1802758551f29abd2d
EunjiHwang <1101j@naver.com>
2021-07-22 15:50:51 +0900

    1011 문자 1개 입력받아 그대로 출력하기

---
+++ charScanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		char x;
+		
+		Scanner scanner = new Scanner(System.in);
+		x = scanner.next().charAt(0);
+		
+		System.out.printf("%c", x);
+	}
+
+}
