e8d7b36407afa034af25053664b136a5c71cfd25
EunjiHwang <1101j@naver.com>
2021-07-23 14:33:44 +0900

    1015 실수 입력받아 둘 째 자리까지 출력하기

---
+++ floatScanner2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		float x;
+		
+		Scanner scanner = new Scanner(System.in);
+		x = scanner.nextFloat();
+		
+		System.out.printf("%.2f", x);
+	}
+
+}
