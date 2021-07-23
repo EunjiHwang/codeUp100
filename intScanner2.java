31a6c1309dce35135ad61c11163032b7d55fba92
EunjiHwang <1101j@naver.com>
2021-07-23 14:35:46 +0900

    1017 정수 1개 입력받아 3번 출력하기

---
+++ intScanner2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int a;
+		
+		Scanner scanner = new Scanner(System.in);
+		a = scanner.nextInt();
+		
+		System.out.printf("%d %d %d", a, a, a);
+	}
+
+}
