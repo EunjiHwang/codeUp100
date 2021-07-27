ba0456e81ec880c959b886dfb03bb1f69cf6279c
EunjiHwang <1101j@naver.com>
2021-07-27 14:13:16 +0900

    1037 정수 입력받아 아스키 문자로 출력하기

---
+++ ascii2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		char b = (char)a;
+		System.out.println(b);
+	}
+
+}
